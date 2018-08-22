cc410c79d58ae3d4a7b1c0ed66b7be54614f08ff
Carson Howard <cjhoward92@gmail.com>
2018-08-21 20:50:12 -0700

    Added a more complex example

--- Program.fs
+++ Program.fs
@@ -10,14 +10,26 @@ let strResult s =
     | "world" -> Ok("hello")
     | _ -> Error("String is not hello or world")
 
+type MatchResult<'a> = {
+    Result: 'a;
+};
+
+let matcher res: Result<MatchResult<'a>, 'b> =
+    match res with
+    | Ok(value) -> Ok({ Result = value })
+    | Error(msg) -> Error(msg)
+
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
 
+    // Can now use the comp expr to chain result calls
+    // Makes them a whole lot cleaner
     result {
         let! x = numResult 101
         let! y = strResult "hello"
-        return (x, y)
+        let! z = matcher(y |> Ok)
+        return (x, y, z)
     } |> printfn "%A\n"
 
     0 // return an integer exit code
